32284eba32100c428e2cde50bcd9d3064eb1eb59
Ikram Shaikh <ikramshaikh0480@gmail.com>
2024-07-09 09:50:55 +0530

    added a add button in groceryListItem to navigate to newItem also make groceriesList widget to StatefulWidget

--- lib/screens/groceries_list.dart
+++ lib/screens/groceries_list.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/screens/new_item.dart';
 
-class GroceriesListScreen extends StatelessWidget {
+class GroceriesListScreen extends StatefulWidget {
   const GroceriesListScreen({super.key});
+
+  @override
+  State<GroceriesListScreen> createState() => _GroceriesListScreenState();
+}
+
+class _GroceriesListScreenState extends State<GroceriesListScreen> {
+  void _addItem() {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => const NewItemScreen(),
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Your Groceries',
-          style: Theme.of(context)
-              .textTheme
-              .titleLarge!
-              .copyWith(fontWeight: FontWeight.w500),
-        ),
+        title: const Text('Your Groceries'),
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(Icons.add),
+          )
+        ],
       ),
       body: ListView.builder(
         itemCount: groceryItems.length,
