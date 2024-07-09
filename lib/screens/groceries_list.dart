4e04dbb89ead68139f8d3bc51563afbc6b40944a
Ikram Shaikh <ikramshaikh0480@gmail.com>
2024-07-09 09:23:53 +0530

    change a file name

---
+++ lib/screens/groceries_list.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/data/dummy_items.dart';
+
+class GroceriesListScreen extends StatelessWidget {
+  const GroceriesListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Your Groceries',
+          style: Theme.of(context)
+              .textTheme
+              .titleLarge!
+              .copyWith(fontWeight: FontWeight.w500),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: itemList.length,
+        itemBuilder: (BuildContext context, int index) => ListTile(
+          title: Text(
+            itemList[index].name,
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+          leading: Container(
+            width: 25,
+            height: 25,
+            color: itemList[index].category.color,
+          ),
+          trailing: Text('${itemList[index].quantity}',
+              style: Theme.of(context).textTheme.titleMedium),
+        ),
+      ),
+    );
+  }
+}
